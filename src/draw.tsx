fe615885296ef113dda8ee508eb4d5dc07537177
kent <164713332@qq.com>
2026-01-06 11:09:22 +0000

    drawed fretboard.

--- src/draw.tsx
+++ src/draw.tsx
@@ -6,12 +6,18 @@ const App = () => {
     let fretWidth = 80
     let fretHeight = 30
 
-    let notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+    let notes = ['C', '', 'D', '', 'E', 'F', '', 'G', '', 'A', '', 'B']
+    let startNote = [4, 11, 7, 2, 9, 4]
 
     let frets = []
     for (let i = 0; i < 6; i++) {
         for (let j = 0; j < 12; j++) {
-            frets.push(createFret(fretWidth, fretHeight, fretWidth*j, fretHeight*i))
+            let start = startNote[i];
+            let note = notes[start+j]
+            if (note == null) {
+                note = notes[start+j-12]
+            }
+            frets.push(createFret(fretWidth, fretHeight, fretWidth*j, fretHeight*i, note === '' ? null : note))
         }
     }
 
@@ -24,7 +30,7 @@ const App = () => {
     );
 };
 
-function createFret(width: number, height: number, x: number, y: number) {
+function createFret(width: number, height: number, x: number, y: number, note: string|null = null) {
     return <Group x={x} y={y} draggable>
         <Rect
             width={width}
@@ -49,26 +55,31 @@ function createFret(width: number, height: number, x: number, y: number) {
             lineJoin="round"
             offsetY={-height/2}
         />
-        <Circle
-            x={width / 2}
-            y={height / 2}
-            radius={height / 3}
-            fill="red"
-            stroke="black"
-            strokeWidth={0}
-        />
-        <Text
-            width={width}
-            height={height}
-            x={0}
-            y={0}
-            text="X"
-            fontSize={height / 2}
-            fontFamily="Calibri"
-            fill="white"
-            align={"center"}
-            verticalAlign={"middle"}
-        />
+        {note != null && (
+            [
+                <Circle
+                    x={width / 2}
+                    y={height / 2}
+                    radius={height / 3}
+                    fill="red"
+                    stroke="black"
+                    strokeWidth={0}
+                />,
+                <Text
+                width={width}
+                height={height}
+                x={0}
+                y={0}
+                text={note}
+                fontSize={height / 2}
+                fontFamily="Calibri"
+                fill="white"
+                align={"center"}
+                verticalAlign={"middle"}
+                />
+            ]
+
+        )}
     </Group>
 }
 
